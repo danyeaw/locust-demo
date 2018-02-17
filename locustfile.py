8eab4bb1d54aa14fb7079bb4aa8b7633ae5cb03f
Bruce Webber <self@brucewebber.us>
2018-02-16 20:48:24 -0500

    Add load test using Locust

---
+++ locustfile.py
@@ -0,0 +1,25 @@
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+    @task(6)
+    def hello(self):
+        self.client.get('/')
+
+    @task(10)
+    def health(self):
+        self.client.get('/health')
+
+    @task(3)
+    def shorter(self):
+        self.client.get('/shorter')
+
+    @task(1)
+    def longer(self):
+        self.client.get('/longer')
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
